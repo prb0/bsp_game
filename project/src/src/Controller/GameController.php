f0331c8159129c55d94a677aaab811baaf60cc3c
prb0 <artembondar1991@gmail.com>
2018-11-19 03:37:22 +0300

    added popup(magnific)

--- project/src/src/Controller/GameController.php
+++ project/src/src/Controller/GameController.php
@@ -21,7 +21,7 @@ class GameController extends AbstractController
      */
     public function findenemy()
     {
-        sleep(random_int(3, 10));
+        sleep(random_int(1, 3));
         return $this->json(['success' => true]);
     }
 
@@ -35,15 +35,15 @@ class GameController extends AbstractController
         if (random_int(0, 9) > 2) {
             switch ($choosen) {
                 case 'Камень':
-                    $response = ['success' => true, 'message' => 'Поздравляем, вы победили!', 'opponent' => 'Ножницы'];
+                    $response = ['success' => true, 'message' => 'Поздравляем, вы победили!', 'opponent' => 'Ножницы', 'bonus' => 100];
                     break;
 
                 case 'Ножницы':
-                    $response = ['success' => true, 'message' => 'Поздравляем, вы победили!', 'opponent' => 'Бумага'];
+                    $response = ['success' => true, 'message' => 'Поздравляем, вы победили!', 'opponent' => 'Бумага', 'bonus' => 100];
                     break;
 
                 case 'Бумага':
-                    $response = ['success' => true, 'message' => 'Поздравляем, вы победили!', 'opponent' => 'Камень'];
+                    $response = ['success' => true, 'message' => 'Поздравляем, вы победили!', 'opponent' => 'Камень', 'bonus' => 100];
                     break;
                 
                 default:
@@ -52,19 +52,19 @@ class GameController extends AbstractController
             }
         } else {
             if (random_int(0, 1)) {
-                $response = ['success' => true, 'message' => 'Ничья!', 'opponent' => $choosen];
+                $response = ['success' => true, 'message' => 'Ничья!', 'opponent' => $choosen, 'bonus' => '0'];
             } else {
                 switch ($choosen) {
                     case 'Камень':
-                        $response = ['success' => true, 'message' => 'К сожалению, вы проиграли :(', 'opponent' => 'Бумага'];
+                        $response = ['success' => true, 'message' => 'К сожалению, вы проиграли :(', 'opponent' => 'Бумага', 'bonus' => '-100'];
                         break;
     
                     case 'Ножницы':
-                        $response = ['success' => true, 'message' => 'К сожалению, вы проиграли :(', 'opponent' => 'Камень'];
+                        $response = ['success' => true, 'message' => 'К сожалению, вы проиграли :(', 'opponent' => 'Камень', 'bonus' => '-100'];
                         break;
     
                     case 'Бумага':
-                        $response = ['success' => true, 'message' => 'К сожалению, вы проиграли :(', 'opponent' => 'Ножницы'];
+                        $response = ['success' => true, 'message' => 'К сожалению, вы проиграли :(', 'opponent' => 'Ножницы', 'bonus' => '-100'];
                         break;
                     
                     default:
@@ -74,7 +74,7 @@ class GameController extends AbstractController
             }
         }
 
-        sleep(random_int(3, 10));
+        sleep(random_int(1, 3));
         return $this->json($response);
     }
 }
